8e2560283802855c39bf3630c5752b60d8479ebf
Ermens <juuloploo@gmail.com>
2022-05-04 20:05:58 +0200

    Completely changed everything to my own version

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# RockPaperScissor-OpenCV
+# RockPaperScissors-OpenCV
 
-This is a simple rock paper scissor game, made using python, OpenCV and mediapipe for hand-tracking.
-
-This contains a HandTrackingModule which makes working with hand-tracking easy, it has methods - findHands to track hands, findPosition to get coordinates i.e. hand landmarks for all 21 points that mediapipe offers for a hand, and a fingersUp method that returns how many fingers are currently open. 
-
-The fingersUp method is used to know whether the player has played rock, paper or scissor - rock for 0 fingers open, scissor for 2 fingers open and paper for all five fingers open.
-
-https://user-images.githubusercontent.com/85190369/166179806-76b8cf07-1dcf-4515-a1fc-24aec34b4147.mov
-
-
-
-There is a red area on the player side and only when the player shows his move inside the area, it is considered a move. There is a timer which tells when the computer will play its move, it resets after every 4 seconds and after the player has played his move, if the timer hits 0 before the player's move, it will wait for the player to play. The computer will play when the timer hits 0 and the player move is detected.
-
-Scores for both player and computer are shown below their sides.
-
-
-https://user-images.githubusercontent.com/85190369/166187639-29a0289c-ec05-4840-819f-beef6bdbec24.mp4
-
-
-For accurately showcasing your move, don't wait for the timer to hit 0 to play your move, be ready with your move in the red area before the timer hits 0.
-
-There is no end condition for the game, you can play it for as long as you want.
-
-
-https://user-images.githubusercontent.com/85190369/166200684-2fadb07c-d409-4885-a150-6b7abb430d3a.mp4
-
+This is a simple rock, paper, scissors where the a webcam with hand-tracking is used to play against a bot. It uses the OpenCV vision library in Python.
--- game.py
+++ game.py
@@ -1,105 +1,129 @@
-import random
-
-import cv2
+import cvzone
+import cv2 as cv
+from isodate import time_isoformat
 import HandTrackingModule as htm
-import time
+from time import time
+from random import randint
 
+# Constants
+WAIT_TIME = 3
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+ORANGE = (150, 150, 0)
+FONT = cv.FONT_HERSHEY_PLAIN
+WEBCAM = cv.VideoCapture(0) 
+WEBCAM.set(3, WINDOW_WIDTH)
+WEBCAM.set(4, WINDOW_HEIGHT)
+HAND_DECTECTOR = htm.handDetector(detectionCon=0.8, maxHands=1)
+IMG_ROCK = cv.imread(f'images/rock.png', cv.IMREAD_UNCHANGED)
+IMG_PAPER = cv.imread(f'images/paper.png', cv.IMREAD_UNCHANGED)
+IMG_SCISSORS = cv.imread(f'images/scissors.png', cv.IMREAD_UNCHANGED)
 
-def checkWinner(player, comp):
-    if player == comp:
-        return None
-    elif player == 'rock' and comp == 'scissor':
-        return 0
-    elif player == 'paper' and comp == 'rock':
-        return 0
-    elif player == 'scissor' and comp == 'paper':
-        return 0
+# Variables
+bot_score, player_score = 0, 0
+pre_time = time()
+choice_player, choice_bot = None, None
+choice_bot = ['rock', 'paper', 'scissors'][randint(0, 2)]
+time_left = 5
+
+def check_hand_gesture(fingers_up):
+    if fingers_up == 0:
+        return 'rock'
+    elif fingers_up == 5:
+        return 'paper'
+    elif fingers_up == 2:
+        return 'scissors'
     else:
-        return 1
+        return None
 
+def draw_text(text, text_x, text_y, text_size, color=WHITE):
+    cv.putText(frame, str(text), (text_x, text_y), FONT, text_size, BLACK, 8, cv.LINE_AA)
+    cv.putText(frame, str(text), (text_x, text_y), FONT, text_size, color, 4, cv.LINE_AA)
 
-# VARIABLES
-waitTime = 4
-moves = ['rock', 'paper', 'scissor']
-scores = [0, 0]  # [player, comp]
-comp, player = None, None
-wCam, hCam = 1280, 720
+def check_winner(choice_bot, choice_player):
+    if choice_player == None or choice_bot == None:
+        return None
+    elif choice_player == choice_bot:
+        return 'draw'
+    elif choice_player == 'rock' and choice_bot == 'scissors':
+        return 'player'
+    elif choice_player == 'paper' and choice_bot == 'rock':
+        return 'player'
+    elif choice_player == 'scissors' and choice_bot == 'paper':
+        return 'player'
+    else:
+        return 'bot'
 
-# Get feed
-cap = cv2.VideoCapture(0)
-cap.set(3, wCam)
-cap.set(4, hCam)
-
-# Get detector
-detector = htm.handDetector(detectionCon=0.8, maxHands=1)
-
-# Time variables for fps and time limit
-pTime = 0
-prevTime = time.time()
-newTime = time.time()
+def draw_gesture(frame, gesture, x, y, flipped=False):
+    if gesture is None:
+        return frame
+    elif gesture == 'rock':
+        gesture_img = IMG_ROCK
+    elif gesture == 'paper':
+        gesture_img = IMG_PAPER
+    elif gesture == 'scissors':
+        gesture_img = IMG_SCISSORS
+    if flipped:
+        gesture_img = cv.flip(gesture_img, 1)
+    new_frame = cvzone.overlayPNG(frame, gesture_img, [x, y])
+    return new_frame
 
 while True:
-    success, img = cap.read()
-    img = cv2.flip(img, 1)
-
-    # Drawing centre line, area for input, and scores
-    cv2.line(img, (wCam // 2, 0), (wCam // 2, hCam), (0, 255, 0), 5)
-    cv2.rectangle(img, (780, 160), (1180, 560), (0, 0, 255), 2)
-    cv2.putText(img, f'{scores[1]}', (320, 640), cv2.FONT_HERSHEY_PLAIN, 5,
-                (0, 250, 0), 3)
-    cv2.putText(img, f'{scores[0]}', (960, 640), cv2.FONT_HERSHEY_PLAIN, 5,
-                (0, 250, 0), 3)
-
-    img = detector.findHands(img)
-
-    lmList = detector.findPosition(img, draw=False)
-
-    # Handling time limit
-    if waitTime - int(newTime) + int(prevTime) < 0:
-        cv2.putText(img, '0', (960, 120), cv2.FONT_HERSHEY_PLAIN, 7,
-                    (0, 0, 255), 3)
+    # Calculate time and FPS
+    cur_time = time()
+    delta_time = cur_time - pre_time
+    pre_time = cur_time
+    if delta_time == 0:
+        fps = 0
     else:
-        cv2.putText(img, f'{waitTime - int(newTime) + int(prevTime)}', (960, 120), cv2.FONT_HERSHEY_PLAIN, 7,
-                    (0, 0, 255), 3)
+        fps = 1 / delta_time
+    if time_left > 0:
+        time_left -= delta_time
+    else:
+        time_left = 0.0
 
-    # Hand landmarks obtained, next
-    if len(lmList) != 0:
+    # Read webcam, find + draw hands and get + update hand landmarks
+    frame = cv.cvtColor(cv.flip(WEBCAM.read()[1], 1), cv.COLOR_BGR2RGB) 
+    frame = HAND_DECTECTOR.find_hands(frame) 
+    landmarks = len(HAND_DECTECTOR.landmarks(frame))
 
-        if newTime - prevTime >= waitTime:
+    # Get player's gesture
+    if landmarks > 0:
+        fingers_up = HAND_DECTECTOR.get_fingers_up()
+        if time_left > 0:
+            choice_player = check_hand_gesture(fingers_up)
+    
+    # Draw text ang gestuers on frame
+    draw_text(f'FPS: {int(fps)}', 20, 40, 1.9)
+    draw_text(f"Player's choice:", 850, 270, 2.5)
+    frame = draw_gesture(frame, choice_player, 910, 270, True)
+    if time_left > 0:
+        draw_text(f'Countdown: {round(time_left, 1)}s'.center(51), -120, 150, 3.5)
+    else:
+        draw_text(f"Bots's choice:", 100, 270, 2.5)
+        frame = draw_gesture(frame, choice_bot, 150, 270)  
+        winner = check_winner(choice_bot, choice_player)
+        draw_text(f"Press 'R' to restart", 430, 650, 2.7)
+        if winner == 'player':
+            draw_text(f'You won!'.center(51), -120, 150, 3.5, GREEN)
+        elif winner == 'bot':
+            draw_text(f'You lost...'.center(51), -110, 150, 3.5, RED)
+        elif winner == 'draw':
+            draw_text(f'Draw'.center(51), -120, 150, 3.5, ORANGE)
+        else:
+            draw_text(f'You did not have a gesture up...'.center(51), -120, 150, 3.5, RED)
 
-            x, y = lmList[0][1:]
-
-            if 780 < x < 1180 and 160 < y < 560:
-                fingers = detector.fingersUp()
-                totalFingers = fingers.count(1)
-
-                # Game logic
-                if totalFingers == 0:
-                    player = 'rock'
-                elif totalFingers == 2:
-                    player = 'scissor'
-                elif totalFingers == 5:
-                    player = 'paper'
-
-                comp = moves[random.randint(0, 2)]
-
-                winner = checkWinner(player, comp)
-                if winner is not None:
-                    scores[winner] = scores[winner] + 1
-
-                prevTime = time.time()
-
-    # Show computer move
-    if comp:
-        img[160:560, 120:520] = cv2.imread(f'Fingers/{comp}.jpg')
-
-    # Show fps
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (1050, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-
-    newTime = time.time()
-
-    cv2.imshow('Image', img)
-    cv2.waitKey(1)
+    # Update frame
+    cv.imshow('Rock Paper Scissors', cv.cvtColor(frame, cv.COLOR_RGB2BGR))
+    
+    # Handle keys
+    key = cv.waitKey(1) 
+    if key == ord('r'): # r key to restart
+        time_left = WAIT_TIME
+        choice_bot = ['rock', 'paper', 'scissors'][randint(0, 2)]
+    elif  key == 27: # 27 = escape key
+        cv.destroyAllWindows()
+        break # exit script
